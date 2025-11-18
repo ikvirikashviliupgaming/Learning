7345dcd6dc66f9b0374493c287a9f0026d6979b9
ikvirikashviliupgaming <ikvirikashvili@gmail.com>
2025-11-18 17:41:36 +0400

    added animatedsprite and x, y, width, height, alpha, visible, rotation, scale, pivot and anchor

--- src/game1/index.ts
+++ src/game1/index.ts
@@ -1,4 +1,4 @@
-import { Application, Assets } from "pixi.js";
+import { AnimatedSprite, Application, Assets, Texture } from "pixi.js";
 import { SpineView } from "../spine/spine.view";
 import "@esotericsoftware/spine-pixi-v8";
 import { Container } from "pixi.js";
@@ -14,7 +14,6 @@ p.personalNumber = 90;
 p.logMyHairColor();
 
 const head = new Head();
-
 
 const app = new Application();
 
@@ -36,7 +35,49 @@ async function main() {
     alias: "spineAtlas",
     src: "/common/test/celestial-circus-pro.atlas",
   });
-  await Assets.load(["spineData", "spineAtlas"]);
+  Assets.add({
+    alias: "card",
+    src: "/game1/card/card.json",
+  });
+  const resources = await Assets.load(["spineData", "spineAtlas", "card"]);
+  console.warn(resources);
+
+  const cardTextures: Texture[] = Object.values(resources.card.textures);
+  console.error(cardTextures);
+
+  const animatedSprite = new AnimatedSprite(cardTextures);
+  app.stage.addChild(animatedSprite);
+  // animatedSprite.play();
+  // animatedSprite.loop = false;
+  // animatedSprite.animationSpeed = 0.1;
+  // animatedSprite.gotoAndPlay(12);
+  // setTimeout(() => {
+  //   animatedSprite.stop();
+  // }, 2000);
+
+  animatedSprite.gotoAndStop(52);
+
+  animatedSprite.x = 200;
+  animatedSprite.y = 190;
+  animatedSprite.position.set(200, 190);
+  animatedSprite.position.x = 200;
+  animatedSprite.position.y = 200;
+
+  // animatedSprite.alpha = 0.5; // 0 - 1;
+  // animatedSprite.visible = true; // false
+  // animatedSprite.rotation = 0; //Math.PI;
+
+  // animatedSprite.pivot.set(100, 100); // pixel
+  // animatedSprite.pivot.x = 100;
+  // animatedSprite.pivot.y = 100;
+
+  // animatedSprite.anchor.set(0.5, 0.5); // %
+  // animatedSprite.anchor.x = 0.5;
+  // animatedSprite.anchor.y = 0.5;
+
+  animatedSprite.scale.set(0.5);
+
+  // animatedSprite.pivot.set()
 
   //
 
@@ -44,25 +85,25 @@ async function main() {
   app.stage.addChild(myContainer);
   myContainer.label = "my first container";
 
-  const mySprite = new Sprite(); // img
-  app.stage.addChild(mySprite);
+  // const mySprite = new Sprite(); // img
+  // app.stage.addChild(mySprite);
 
-  const myGraphics = new Graphics(); // svg
-  myGraphics.circle(0, 0, 100);
-  myGraphics.fill(0xff0000);
-  myGraphics.stroke({ color: 0x0000ff, width: 2 });
-  app.stage.addChild(myGraphics);
-  myGraphics.position.set(150, 150);
+  // const myGraphics = new Graphics(); // svg
+  // myGraphics.circle(0, 0, 100);
+  // myGraphics.fill(0xff0000);
+  // myGraphics.stroke({ color: 0x0000ff, width: 2 });
+  // app.stage.addChild(myGraphics);
+  // myGraphics.position.set(150, 150);
 
-  const myText = new Text({
-    // text
-    text: "I am First text in PIXIJS",
-    style: {
-      fill: 0xffff00,
-    },
-  });
-  app.stage.addChild(myText);
-  myText.position.set(200, 50);
+  // const myText = new Text({
+  //   // text
+  //   text: "I am First text in PIXIJS",
+  //   style: {
+  //     fill: 0xffff00,
+  //   },
+  // });
+  // app.stage.addChild(myText);
+  // myText.position.set(200, 50);
 
   // const g = new Graphics();
   // g.rect(0, 0, 200, 100);
